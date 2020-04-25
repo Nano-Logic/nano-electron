05393883f5b268b2368994cd2cab42f77746015f
MsFire <learntv7@gmail.com>
2020-04-25 09:17:46 +0200

    Updates

--- index.js
+++ index.js
@@ -9,9 +9,22 @@ const {
     app
 } = require('electron');
 
-var window = new ElectronWindow(540, 280, 'pages/main.html');
+var window = new ElectronWindow(540, 280, 'pages/main.html', null /** No template */ , true);
 
 app.on('ready', () => {
 
     window.loadWindow();
+
+    /**
+     * Change the @func window.defaultTemplate(window.mainWindow) for different custom template.
+     * Loads default Template. 
+     */
+    window.loadTemplate(window.defaultTemplate(window.mainWindow));
+
+    /**
+     * Closes application.
+     */
+    window.mainWindow.on('closed', () => {
+        app.quit();
+    });
 });
